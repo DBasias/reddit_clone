9737b770cb069bd973d187bde1b9057fddd54a8b
DBasias <dbasiadimas@hotmail.com>
2023-09-11 12:32:41 +0300

    can get community data without going to community page first

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -10,14 +10,17 @@ import { auth, firestore } from "@/src/firebase/clientApp";
 import {
   collection,
   doc,
+  getDoc,
   getDocs,
   increment,
   writeBatch,
 } from "firebase/firestore";
 import { authModalAtom } from "../atoms/authModalAtom";
+import { useRouter } from "next/router";
 
 const useCommunityData = () => {
   const [user] = useAuthState(auth);
+  const router = useRouter();
   const [communityStateValue, setCommunityStateValue] =
     useRecoilState(communityState);
   const setModalAuthState = useSetRecoilState(authModalAtom);
@@ -122,6 +125,23 @@ const useCommunityData = () => {
     setLoading(false);
   };
 
+  const getCommunityData = async (communityId: string) => {
+    try {
+      const communityDocRef = doc(firestore, "communities", communityId);
+      const communityDoc = await getDoc(communityDocRef);
+
+      setCommunityStateValue(prev => ({
+        ...prev,
+        currentCommunity: {
+          id: communityDoc.id,
+          ...communityDoc.data(),
+        } as Community,
+      }));
+    } catch (error: any) {
+      console.log("getCommunityData error:", error.message);
+    }
+  };
+
   useEffect(() => {
     if (!user) {
       setCommunityStateValue(prev => ({
@@ -132,6 +152,14 @@ const useCommunityData = () => {
     }
     getMySnippets();
   }, [user]);
+
+  useEffect(() => {
+    const { communityId } = router.query;
+
+    if (communityId && !communityStateValue.currentCommunity) {
+      getCommunityData(communityId as string);
+    }
+  }, [router.query, communityStateValue.currentCommunity]);
 
   return {
     communityStateValue,
